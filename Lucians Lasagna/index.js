f49f60a36cfb3bf3a3981ed7a6feab034bd93740
Hrafnagud <joao.davi12121@gmail.com>
2022-04-20 21:57:56 -0300

    Lucians Lasagna

---
+++ Lucians Lasagna/index.js
@@ -0,0 +1,17 @@
+const EXPECTED_MINUTES_IN_OVEN = 40;
+const PREPARATION_MINUTES_PER_LAYER = 2;
+
+ function remainingMinutesInOven(actualMinutesInOven) {
+  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
+}
+
+function preparationTimeInMinutes(numberOfLayers) {
+  return PREPARATION_MINUTES_PER_LAYER * numberOfLayers;
+}
+
+function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
+  return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
+
+}
+
+console.log(totalTimeInMinutes(5,20));
